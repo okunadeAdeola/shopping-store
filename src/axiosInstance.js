db5e5f75c1f57f72741179925eab3cac122faabd
okunade olaitan <olaokunade@gmail.com>
2025-03-01 19:57:00 +0000

    first commit

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: 'https://shopsphere-node.onrender.com',
+    baseURL: 'https://shopping-backend-5jrm.onrender.com',
     // baseURL: 'http://localhost:3000',
     headers: {
         'Content-Type': 'application/json',
