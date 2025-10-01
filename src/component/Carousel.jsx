82f704e9331571502c58fb108e4743d9e3d673ea
okunade olaitan <olaokunade@gmail.com>
2025-10-01 17:29:11 +0000

    first commit

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -6,8 +6,8 @@ const Carousel = () => {
   return (
     <div className="">
       <div className='bg-black space-x-5 text-white p-2 text-center'>
-        <span className='animate animate-pulse'>
-          Discover our latest collection of exclusive products! Enjoy special discounts and offers tailored just for you. Shop now and elevate your style!
+        <span className='animate-pulse'>
+          Discover our latest collection of exclusive products! Enjoy special discounts and offers simulated just for you. Shop now and elevate your style!
         </span>
         <span>
           <Link className='underline' to="">Log in</Link>
